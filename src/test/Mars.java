06849d6d3eeb115c25c58306d80f3cadd920ddad
kanat17990 <kussenov89@gmail.com>
2020-02-04 21:03:16 -0500

    Mars class added

--- src/test/Mars.java
+++ src/test/Mars.java
@@ -3,8 +3,16 @@ package test;
 public class Mars {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("Hello World");
 
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 }
